9556ad1107e1023b481f0535b3bc40369baf58da
aranelpalantir <trmert@hotmail.com>
2024-07-02 13:02:20 +0300

    Refactor connection string creation to use SqlConnectionStringBuilder

--- Models/DatabaseConnectionInfo.cs
+++ Models/DatabaseConnectionInfo.cs
@@ -1,10 +1,26 @@
-﻿namespace MssqlToolBox.Models
+﻿using Microsoft.Data.SqlClient;
+
+namespace MssqlToolBox.Models
 {
     internal record DatabaseConnectionInfo(string Server, string Username, string Password)
     {
         public string Server { get; set; } = Server;
         public string Username { get; set; } = Username;
         public string Password { get; set; } = Password;
-        public string ConnectionString => $"Data Source={Server};User ID={Username};Password={Password};TrustServerCertificate=true;";
+        public string ConnectionString
+        {
+            get
+            {
+                var builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = Server,
+                    UserID = Username,
+                    Password = Password,
+                    TrustServerCertificate = true
+                };
+
+                return builder.ConnectionString;
+            }
+        }
     }
 }
